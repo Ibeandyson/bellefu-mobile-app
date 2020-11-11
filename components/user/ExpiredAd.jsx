697a1e495a52274d029bc1b0da029e70892dd574
Justice Eziefule <thechiefje@gmail.com>
2020-11-11 16:40:51 +0100

    call api for dashboard

--- components/user/ExpiredAd.jsx
+++ components/user/ExpiredAd.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
+import {View, Text, Image, ScrollView, StyleSheet, Dimensions,} from 'react-native';
 import {Card, Paragraph, Divider} from 'react-native-paper';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {FlatList} from 'react-native-gesture-handler';
@@ -8,6 +8,7 @@ import {useSelector} from 'react-redux';
 import axios from 'axios';
 import Preloader from '../guest/Preloader';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AdTableItem from './AdTableItem';
 
 export default function ExpiredAd(props) {
     const [ad, setAd] = useState([]);
@@ -40,6 +41,12 @@ export default function ExpiredAd(props) {
         }
     };
 
+    const onAdDelete = (slug) => {
+		setAd((ads) =>
+      	ads.filter((ad) => ad.slug !== slug)
+    );
+    }
+
     const loadAd = async() => {
         let tokenn = await AsyncStorage.getItem('user')
         await setToken(tokenn)
@@ -52,7 +59,6 @@ export default function ExpiredAd(props) {
                 }
             })
             .then(res => {
-                console.log(res.data.products.data);
                 setProducts(res.data.products);
                 setAd(res.data.products.data);
                 setLoading(false);
@@ -75,11 +81,11 @@ export default function ExpiredAd(props) {
     return (
         <View>
            
-                <View style={{marginBottom: 200}}>
+                <View>
                     {loading ? (
                         <Preloader />
                     ) : status ? (
-                        <View style={{marginTop: 50, flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                        <View style={{marginTop: 50, flex: 1, height: Dimensions.get('window').height, justifyContent: 'center', alignItems: 'center'}}>
                             <Text style={{fontSize: 20}}>No Item</Text>
                         </View>
                     ) : (
@@ -88,51 +94,8 @@ export default function ExpiredAd(props) {
                             keyExtractor={item => item.slug}
                             onEndReached={nextData}
                             onEndReachedThreshold={1}
-                            renderItem={({item, index}) => (
-                                <Card key={item.slug} style={{marginTop: 5, borderRadius: 0}}>
-                                    <View style={styles.img}>
-                                        <TouchableOpacity onPress={() => props.navigation.navigate('Detail', {item})}>
-                                            <Image
-                                                style={{height: 120, width: 150}}
-                                                source={{
-                                                    uri: `https://bellefu.com/images/products/${item.slug}/${item
-                                                        .images[0]}`
-                                                }}
-                                            />
-                                        </TouchableOpacity>
-                                        <View style={styles.writUp}>
-                                            <Paragraph style={styles.title} ellipsizeMode="tail" numberOfLines={2}>
-                                                {item.title}
-                                            </Paragraph>
-                                            <Divider />
-                                            <Paragraph style={styles.price}>
-                                                <Text style={{color: '#76ba1b', fontWeight: '900'}}>
-                                                    {item.currency_symbol}
-                                                    {item.price}
-                                                </Text>
-                                            </Paragraph>
-                                            <Divider />
-                                            <View style={{flexDirection: 'row'}}>
-                                                <Paragraph style={{fontWeight: 'bold', marginLeft: 20}}>
-                                                    Posted:
-                                                </Paragraph>
-                                                <Paragraph style={{marginLeft: 5}}>2 day ago</Paragraph>
-                                                <Text style={{marginLeft: 5}}>
-                                                    {item.created_at}
-                                                </Text>
-                                            </View>
-                                            <Divider />
-                                            <View style={styles.icons}>
-                                                <View style={styles.convert}>
-                                                    <AntDesign name="edit" size={25} color="green" />
-                                                </View>
-                                                <View style={styles.like}>
-                                                    <AntDesign name="delete" size={20} color="red" />
-                                                </View>
-                                            </View>
-                                        </View>
-                                    </View>
-                                </Card>
+                            renderItem={({item}) => (
+                                <AdTableItem styles={styles} item={item} onAdDelete={onAdDelete} token={token} key={item.slug} />
                             )}
                         />
                     )}
@@ -163,7 +126,7 @@ const styles = StyleSheet.create({
     icons: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'flex-start'
     },
     like: {
         paddingLeft: 100,
